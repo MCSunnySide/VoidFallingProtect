d8abaad63a87fd3bf60c11ac1d6aba213ea13a2b
Ghost_chu <2908803755@qq.com>
2020-06-27 14:52:26 +0800

    Init

---
+++ src/main/java/com/mcsunnyside/voidfallingprotect/VoidFallingProtect.java
@@ -0,0 +1,49 @@
+package com.mcsunnyside.voidfallingprotect;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VoidFallingProtect extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(this,this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onPlayerMove(PlayerMoveEvent event){
+        if(event instanceof PlayerTeleportEvent){
+            return;
+        }
+        if(event.getTo() == null){
+            return;
+        }
+        if(event.getTo().getBlockY() >= 0){ //忽略玩家正常移动
+            return;
+        }
+        if(event.getFrom().getBlockY() < 0){ //忽略玩家已经在低处移动
+            return;
+        }
+        if(!event.getFrom().getBlock().getType().isSolid()){ //忽略玩家通过不完整方块正常离开世界范围
+            return;
+        }
+        teleportToSafeLoc(event.getPlayer());
+        event.setCancelled(true);
+    }
+
+    public void teleportToSafeLoc(Player player){
+        player.sendMessage(ChatColor.YELLOW+"检测到不安全的移动，已传送你到安全的地点。");
+        player.teleport(player.getWorld().getSpawnLocation());
+    }
+}
